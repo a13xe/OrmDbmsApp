b988c9f95e5dce08da5a80dcbbccf5d8927ca737
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-06-03 00:39:30 +0300

    sql with test data generated

--- SQL/db_with_test_data_02.sql
+++ SQL/db_with_test_data_02.sql
@@ -178,3 +178,129 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--  __                                            __              __                            __                  __              __                --
+-- /  |                                          /  |            /  |                          /  |                /  |            /  |               --
+-- $$/  _______    _______   ______    ______   _$$ |_          _$$ |_     ______    _______  _$$ |_           ____$$ |  ______   _$$ |_     ______   --
+-- /  |/       \  /       | /      \  /      \ / $$   |        / $$   |   /      \  /       |/ $$   |         /    $$ | /      \ / $$   |   /      \  --
+-- $$ |$$$$$$$  |/$$$$$$$/ /$$$$$$  |/$$$$$$  |$$$$$$/         $$$$$$/   /$$$$$$  |/$$$$$$$/ $$$$$$/         /$$$$$$$ | $$$$$$  |$$$$$$/    $$$$$$  | --
+-- $$ |$$ |  $$ |$$      \ $$    $$ |$$ |  $$/   $$ | __         $$ | __ $$    $$ |$$      \   $$ | __       $$ |  $$ | /    $$ |  $$ | __  /    $$ | --
+-- $$ |$$ |  $$ | $$$$$$  |$$$$$$$$/ $$ |        $$ |/  |        $$ |/  |$$$$$$$$/  $$$$$$  |  $$ |/  |      $$ \__$$ |/$$$$$$$ |  $$ |/  |/$$$$$$$ | --
+-- $$ |$$ |  $$ |/     $$/ $$       |$$ |        $$  $$/         $$  $$/ $$       |/     $$/   $$  $$/       $$    $$ |$$    $$ |  $$  $$/ $$    $$ | --
+-- $$/ $$/   $$/ $$$$$$$/   $$$$$$$/ $$/          $$$$/           $$$$/   $$$$$$$/ $$$$$$$/     $$$$/         $$$$$$$/  $$$$$$$/    $$$$/   $$$$$$$/  --
+--                                                                                                                                                    --
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+-- Insert data into `brand` table
+INSERT INTO `pc`.`brand` (`name`) VALUES
+('ASRock'),
+('ASUS'),
+('Gigabyte'),
+('MSI'),
+('AMD'),
+('INTEL'),
+('NVIDIA');
+
+
+-- Insert data into `chipset` table
+INSERT INTO `pc`.`chipset` (`name`) VALUES
+('H460'),
+('H410'),
+('H310'),
+('B550'),
+('B450'),
+('B365'),
+('B360'),
+('A520'),
+('A320'),
+('A68H');
+
+
+-- Insert data into `socket` table
+INSERT INTO `pc`.`socket` (`name`) VALUES
+('LGA 1151-v2'),
+('LGA 1200'),
+('AM4'),
+('FM2+');
+
+
+-- Insert data into `socket_to_chipset` table
+INSERT INTO `pc`.`socket_to_chipset` (`socket_id`, `chipset_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+
+-- Insert data into `cpu` table
+INSERT INTO `pc`.`cpu` (`model`, `price`, `cores`, `threads`, `frequency`, `brand_id`, `socket_id`) VALUES
+('Intel Core i7-9700K', 359.99, 8, 8, 3.6, 6, 1),
+('AMD Ryzen 7 5800X', 449.99, 8, 16, 3.8, 5, 3),
+('Intel Core i5-10400F', 179.99, 6, 12, 2.9, 6, 2),
+('AMD Ryzen 5 3600', 199.99, 6, 12, 3.6, 5, 3),
+('Intel Core i9-9900K', 499.99, 8, 16, 3.6, 6, 1),
+('AMD Ryzen 9 5950X', 799.99, 16, 32, 3.4, 5, 3),
+('Intel Core i7-10700K', 349.99, 8, 16, 3.8, 6, 2),
+('AMD Ryzen 7 3700X', 329.99, 8, 16, 3.6, 5, 3),
+('Intel Core i5-11600K', 269.99, 6, 12, 3.9, 6, 2),
+('AMD Ryzen 5 5600X', 299.99, 6, 12, 3.7, 5, 3),
+('Intel Core i3-10100', 139.99, 4, 8, 3.6, 6, 2),
+('AMD Ryzen 3 3300X', 139.99, 4, 8, 3.8, 5, 3),
+('Intel Pentium Gold G6400', 89.99, 2, 4, 4.0, 6, 2),
+('AMD Athlon 3000G', 69.99, 2, 4, 3.5, 5, 3),
+('Intel Celeron G5920', 59.99, 2, 2, 3.5, 6, 2),
+('AMD Athlon 200GE', 54.99, 2, 4, 3.2, 5, 3),
+('Intel Xeon W-3175X', 2999.99, 28, 56, 3.1, 6, 1),
+('AMD Threadripper 3990X', 4299.99, 64, 128, 2.9, 5, 3),
+('Intel Core i9-11900K', 549.99, 8, 16, 3.5, 6, 2);
+
+
+-- Insert data into `gpu` table
+INSERT INTO `pc`.`gpu` (`model`, `price`, `cores`, `memory`, `frequency`, `brand_id`) VALUES
+('NVIDIA GeForce RTX 3080', 699.99, 8704, 10, 1.71, 7),
+('AMD Radeon RX 6800 XT', 649.99, 4608, 16, 2.25, 5),
+('NVIDIA GeForce RTX 3060', 329.99, 3584, 12, 1.78, 7),
+('ASUS GeForce GTX 1650', 169.99, 896, 4, 1.66, 2),
+('NVIDIA GeForce RTX 3090', 1499.99, 10496, 24, 1.70, 7),
+('AMD Radeon RX 6900 XT', 999.99, 5120, 16, 2.25, 5),
+('NVIDIA GeForce RTX 3070', 499.99, 5888, 8, 1.50, 7),
+('ASUS GeForce GTX 1660 Super', 239.99, 1408, 6, 1.78, 2),
+('NVIDIA GeForce RTX 3060 Ti', 399.99, 4864, 8, 1.67, 7),
+('AMD Radeon RX 6700 XT', 579.99, 2560, 12, 2.45, 5),
+('NVIDIA GeForce RTX 3080 Ti', 1199.99, 10240, 12, 1.67, 7),
+('Gigabyte GeForce GTX 1650 Super', 179.99, 1280, 4, 1.72, 3),
+('AMD Radeon RX 6600 XT', 399.99, 2048, 8, 2.59, 5),
+('NVIDIA GeForce GTX 1660', 229.99, 1408, 6, 1.53, 7),
+('ASUS GeForce GT 1030', 89.99, 384, 2, 1.23, 2),
+('NVIDIA GeForce GT 710', 49.99, 192, 2, 0.954, 7),
+('AMD Radeon RX 550', 99.99, 512, 4, 1.18, 5),
+('NVIDIA GeForce MX350', 159.99, 640, 2, 1.47, 7),
+('MSI GeForce GT 730', 59.99, 384, 2, 0.902, 4);
+
+
+-- Insert data into `mbrd` table
+INSERT INTO `pc`.`mbrd` (`model`, `price`, `brand_id`, `socket_id`, `chipset_id`) VALUES
+('ASRock B550 Pro4', 129.99, 1, 3, 4),
+('MSI B450 TOMAHAWK MAX', 114.99, 4, 3, 5),
+('ASUS ROG Strix B365-F Gaming', 129.99, 2, 1, 6),
+('Gigabyte A520 AORUS ELITE', 79.99, 3, 3, 8),
+('ASRock Z490 Steel Legend', 189.99, 1, 1, 1),
+('MSI MAG B550 TOMAHAWK', 179.99, 4, 3, 4),
+('ASUS PRIME B450-PLUS', 109.99, 2, 3, 5),
+('Gigabyte B365M DS3H', 89.99, 3, 1, 6),
+('ASUS ROG Strix X570-E Gaming', 299.99, 2, 3, 4),
+('MSI MPG B550 GAMING PLUS', 159.99, 4, 3, 4),
+('Gigabyte B450 AORUS ELITE', 119.99, 3, 3, 5),
+('ASRock B460 Steel Legend', 119.99, 1, 1, 1),
+('MSI MAG B550M MORTAR', 149.99, 4, 3, 4),
+('ASUS TUF GAMING B450-PRO', 129.99, 2, 3, 5),
+('Gigabyte B550M AORUS ELITE', 139.99, 3, 3, 4),
+('ASRock H470 Steel Legend', 139.99, 1, 1, 1),
+('MSI MAG B560 TOMAHAWK WIFI', 169.99, 4, 3, 4),
+('ASUS ROG Strix B550-F Gaming', 199.99, 2, 3, 4),
+('Gigabyte B450M DS3H', 79.99, 3, 3, 5);
